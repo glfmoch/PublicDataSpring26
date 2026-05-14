377dc5abef62c327851a7627dfa786b8acc5ab4e
Ryan Mathew <glfmoch777@gmail.com>
2026-05-13 17:12:42 -0700

    Finished D3 HW1 with all files

---
+++ Assignments/D3-HW1/README.md
@@ -0,0 +1,11 @@
+# D3 HW1
+
+## Visualization
+
+![Bar chart showing hours spent gaming each day of the week. Weekend days are notably higher, with Saturday reaching 6 hours and Sunday at 5 hours, while weekdays average around 1-2 hours.](gamingbarchart.png)
+
+## Overview
+
+This bar chart displays the number of hours spent gaming each day of the week for the first week of May. The data is an estimate of my gaming hours each day last week entered manually in Excel, then saved as a CSV for loading into D3.
+
+The dataset has 7 entries for each day, with hours ranging from 1 on busy weekdays to all the way to 5 on Saturday. The weekly average is around 2.7 hours per day. The chart uses a band scale on the x-axis for the categorical days and a linear scale on the y-axis for the hour values, pretty in-line with the provided bar code.
---
+++ Assignments/D3-HW1/main.js
@@ -0,0 +1,67 @@
+    //Declare consts/global variables
+        const margin = 30;
+        const width = 500;
+        const height = 500;
+
+
+    //Load data and related variables
+        d3.csv("gaming_hours.csv").then(data => {
+            console.log("data", data)
+        //format data - coerce hours from string to number
+        data.forEach(d => {
+            d.day = d.day;
+            d.hours = +d.hours;
+        });
+
+        const maxY = d3.max(data, d => d.hours);
+
+
+    //Scales - band scale for days, linear scale for hours
+        const xScale = d3.scaleBand()
+                        .domain(data.map(d => d.day))
+                        .range([margin, width - margin])
+                        .paddingInner(.02);
+
+        const yScale = d3.scaleLinear()
+                        .domain([0, maxY])
+                        .range([height - margin, margin]);
+
+
+    //SVG
+        const svg = d3.select("body")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height);
+
+
+    //Axes - create axes
+        const bottomAxis = d3.axisBottom()
+                             .scale(xScale);
+
+        const leftAxis = d3.axisLeft()
+                           .scale(yScale);
+
+
+    //Bars - rect needs x, y, width, and height
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", d => xScale(d.day))
+            .attr("y", d => yScale(d.hours))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => (height - margin) - yScale(d.hours))
+            .attr("fill", "darkgreen");
+
+
+    //Axes - call axes
+        svg.append("g")
+            .attr("transform", "translate(0," + (height - margin) + ")")
+            .call(bottomAxis);
+
+        svg.append("g")
+            .attr("transform", "translate(" + margin + ",0)")
+            .call(leftAxis);
+
+
+    });
